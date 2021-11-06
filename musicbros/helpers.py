a4241f562467e5b51646b9616d0727ffc7227c45
Ben Rosen <benjamin.j.rosen@gmail.com>
2021-11-06 16:28:55 -0400

    clean up helpers

--- musicbros/helpers.py
+++ musicbros/helpers.py
@@ -1,12 +1,13 @@
 import pickle
 from glob import glob
-from os import walk
+from os import system, walk
 
 from tinytag import TinyTag
 from typer import colors, echo, secho, style
 
 from .config import get_config_option, get_config_options
 
+AUDIO_FILE_TYPES = (".mp3", ".m4a", ".flac")
 COLORS = {
     "blue": colors.BLUE,
     "cyan": colors.CYAN,
@@ -36,7 +37,7 @@ def get_imported_albums():
     return albums
 
 
-def get_album_dirs():
+def get_album_directories():
     return [
         root
         for root, dirs, files in walk(get_config_option("shared_directory"))
@@ -45,45 +46,32 @@ def get_album_dirs():
 
 
 def get_audio_files():
-    audio_file_types = (".mp3", ".m4a", ".flac")
     audio_files = list()
-    for file_type in audio_file_types:
+    for file_type in AUDIO_FILE_TYPES:
         audio_files.extend(glob(file_type))
     return audio_files
 
 
-def get_track_total(album, track):
-    path = f"{album}/{track}"
-    tags = TinyTag.get(path)
-    if tags.track_total or tags.track_total == "0":
-        return int(tags.track_total)
+def get_track_total(tracks):
+    track_total = 0
+    message = None
+    track_totals = {TinyTag.get(track).track_total for track in tracks}
+    if not track_totals:
+        message = "missing"
+    elif len(track_totals) > 1:
+        message = "conflicting"
     else:
-        return tags.track_total
+        track_total = int(next(iter(track_totals)))
+    return track_total, message
 
 
-def check_track_totals(album, tracks):
-    total = 0
-    for track in tracks:
-        track_total = get_track_total(album, track)
-        if not track_total:
-            return -3
-        elif total == 0:
-            total = track_total
-        elif total != track_total:
-            return -1
-    return total
-
-
-def set_quote(album):
-    quote = None
-    if "'" and '"' in album:
-        return quote
+def get_single_or_double_quote(album):
+    if "'" in album and '"' in album:
+        return None
     elif '"' in album:
-        quote = "'"
-        return quote
+        return "'"
     else:
-        quote = '"'
-        return quote
+        return '"'
 
 
 def is_already_imported(album):
@@ -91,108 +79,73 @@ def is_already_imported(album):
 
 
 def import_or_get_errors(album):
-    errors = list()
-    albums = list()
     skipped = False
-    imports = False
-    audio_files = get_audio_files()
-    RESULT = check_track_totals(album, audio_files)
-    QUOTE = set_quote(album)
-    IMPORTED = is_already_imported(album)
-    if len(audio_files) == 0:
-        errors.append(
-            "\n- Folder is empty or audio is in wav format (please wait"
-            f' for sync or resolve manually): "{album}"'
+    imported = False
+    error = None
+    if is_already_imported(album):
+        skipped = True
+        return skipped, imported, error
+    tracks = get_audio_files()
+    track_count = len(tracks)
+    track_total, message = get_track_total(tracks)
+    quote_character = get_single_or_double_quote(album)
+    if not tracks:
+        error = (
+            "Folder is empty or audio is in wav format (please wait for sync or"
+            f" resolve manually): '{album}'"
         )
-    elif RESULT >= 0:
-        if len(audio_files) == RESULT:
-            if not QUOTE:
-                if not IMPORTED:
-                    errors.append(
-                        "\n- Annoyingly named directory (please resolve"
-                        f' manually): "{album}"'
-                    )
-                else:
-                    skipped = True
-            else:
-                if not IMPORTED:
-                    system(f"beet import {QUOTE}{album}{QUOTE}")
-                    imports = True
-                else:
-                    skipped = True
-        elif len(audio_files) > RESULT:
-            if not IMPORTED:
-                errors.append(
-                    "\n- Possible multi-disc album detected (please"
-                    f' resolve manually): "{album}"'
-                )
-                albums.append(album)
-            else:
-                skipped = True
+    elif message == "conflicting":
+        error = (
+            f'Possible multi-disc album detected (please resolve manually): "{album}"'
+        )
+    elif message == "missing":
+        error = (
+            "Album does not contain a track total number"
+            f' (please resolve manually): "{album}"'
+        )
+    elif track_count == track_total:
+        if quote_character:
+            system(f"beet import {quote_character}{album}{quote_character}")
+            imported = True
         else:
-            if not IMPORTED:
-                errors.append(
-                    "\n- Missing tracks (please wait for sync or resolve"
-                    f' manually): "{album}"\n\tTrack total: {RESULT}'
-                )
-                albums.append(album)
-            else:
-                skipped = True
+            error = f'Annoyingly named directory (please resolve manually): "{album}"'
+    elif track_count > track_total:
+        error = (
+            f'Possible multi-disc album detected (please resolve manually): "{album}"'
+        )
     else:
-        if RESULT == -1:
-            if not IMPORTED:
-                errors.append(
-                    "\n- Possible multi-disc album detected (please"
-                    f' resolve manually): "{album}"'
-                )
-                albums.append(album)
-            else:
-                skipped = True
-        elif RESULT == -2:
-            if not IMPORTED:
-                errors.append(
-                    "\n- Filetype(s) not recognized (please resolve"
-                    f' manually): "{album}"'
-                )
-                albums.append(album)
-            else:
-                skipped = True
-        elif RESULT == -3:
-            if not IMPORTED:
-                errors.append(
-                    "\n- Album does not contain a track total number"
-                    f' (please resolve manually): "{album}"'
-                )
-                albums.append(album)
-            else:
-                skipped = True
-    return errors, skipped, imports, albums
+        error = (
+            "Missing tracks (please wait for sync or resolve"
+            f' manually): "{album}"\n\tTrack total: {track_total}'
+        )
+    return skipped, imported, error
 
 
-def import_complete_albums(albums):
-    final_errors = list()
-    final_skipped = 0
-    final_imports = False
-    returns_errors = False
+def import_complete_albums():
+    errors = list()
+    skipped_count = 0
+    imports = False
     bulk_fix_albums = list()
     skip_directories = get_config_option("skip_directories")
-    for album in albums:
+    for album in get_album_directories():
         if skip_directories in album:
             continue
         else:
-            errors, skipped, imports, error_albums = import_or_get_errors(album)
-            if imports:
-                final_imports = True
+            skipped, imported, error = import_or_get_errors(album)
+            if imported:
+                imports = True
             if skipped:
-                final_skipped += 1
-            if len(errors) > 0:
-                for error in errors:
-                    final_errors.append(error)
-                for album in error_albums:
-                    bulk_fix_albums.append(album)
-    print(f"{final_skipped} albums skipped.")
-    if len(final_errors) > 0:
-        for error in final_errors:
-            print(error)
-        returns_errors = True
-    return final_imports, returns_errors, bulk_fix_albums
+                skipped_count += 1
+            if error:
+                errors.append(error)
+                bulk_fix_albums.append(album)
+    print(f"{skipped_count} albums skipped.")
+    for error in errors:
+        color(error, echo=True)
+    return imports, errors, bulk_fix_albums
+
+
+def import_error_albums(albums):
+    for album in albums:
+        quote_character = get_single_or_double_quote(album)
+        system(f"beet import {quote_character}{album}{quote_character}")
--- musicbros/main.py
+++ musicbros/main.py
@@ -1,24 +1,10 @@
-from os import system
-
 from typer import Option, Typer, confirm, echo
 
 from .config import write_config_options
-from .helpers import (
-    get_album_dirs,
-    import_complete_albums,
-    print_config_values,
-    set_quote,
-)
+from .helpers import import_complete_albums, import_error_albums, print_config_values
 from .strip_bracket_years import strip_bracket_years as strip_bracket_years_function
 
 app = Typer(help="CLI for managing the Musicbros audio file archive")
-
-
-def import_error_albums(albums):
-    for album in albums:
-        quote = set_quote(album)
-        system(f"beet import {quote}{album}{quote}")
-    strip_bracket_years_function()
 
 
 @app.command()
@@ -35,17 +21,16 @@ def import_new(strip_years: bool = Option(False, "--strip-years")):
     Copy newly added audio files from Dropbox to your music library location via
     beets
     """
-    echo("Importing new albums...")
-    new_imports, errors, bulk_fix_albums = import_complete_albums(get_album_dirs())
-    if new_imports and strip_years:
+    echo("Importing newly added albums...")
+    imports, errors, bulk_fix_albums = import_complete_albums()
+    if imports and strip_years:
         strip_bracket_years_function()
     elif errors:
         print("No new albums to import.")
-        import_all = confirm(
-            "Would you like to import all albums anyway?",
-        )
-        if import_all:
+        if confirm("Would you like to import all albums anyway?"):
             import_error_albums(bulk_fix_albums)
+            if strip_years:
+                strip_bracket_years_function()
 
 
 @app.command()
