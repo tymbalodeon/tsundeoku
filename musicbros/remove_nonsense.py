ecbff003f35eb2310e9f8157f2782f9faa3ff592
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-01-16 10:58:36 -0500

    greatly simplify remove nonsense file

--- musicbros/remove_nonsense.py
+++ musicbros/remove_nonsense.py
@@ -6,6 +6,31 @@ from typer import echo
 from .helpers import BRACKET_YEAR_REGEX
 
 
+ACTIONS = [
+    (
+        'Removing bracketed years from all "album" tags...',
+        BRACKET_YEAR_REGEX,
+        "",
+        "album",
+        True,
+    ),
+    (
+        'Replacing "Rec.s" with "Recordings" in all "album" tags...',
+        r"\bRec\.s\b",
+        "Recordings",
+        "album",
+        True,
+    ),
+    (
+        'Removing "solo" instrument brackets from all "artist" tags...',
+        r"\s\[solo.+\]",
+        "",
+        "artist",
+        False,
+    ),
+]
+
+
 def list_items(
     query_tag,
     query,
@@ -18,89 +43,33 @@ def list_items(
     return albums
 
 
-def get_album_operation_flag(operate_on_albums):
-    return "-a" if operate_on_albums else ""
-
-
 def beet_modify(confirm, operate_on_albums, modify_tag, found, replacement):
     run(
         [
             "beet",
             "modify",
             "" if confirm else "-y",
-            get_album_operation_flag(operate_on_albums),
+            "-a" if operate_on_albums else "",
             f"{modify_tag}::^{found}$",
             f"{modify_tag}={replacement}",
         ]
     )
 
 
-def update_tag(find_regex, replacement, tags, confirm, operate_on_albums, modify_tag):
-    tags = [(escape(tag), sub(find_regex, replacement, tag)) for tag in tags]
-    for found_value, replacement_value in tags:
-        beet_modify(
-            confirm, operate_on_albums, modify_tag, found_value, replacement_value
-        )
-
-
-def update_tags(
-    query_regex,
-    query_tag,
-    modify_tag,
-    find_regex,
-    replacement,
-    operate_on_albums,
-    confirm,
-):
-    tags = list_items(query_tag, query_regex, operate_on_albums)
-    update_tag(
-        find_regex, replacement, tags, confirm, operate_on_albums, modify_tag
-    ) if tags else echo("No albums to update.")
-
-
 def replace_tags(message, find, replace, tag, operate_on_albums):
     echo(message)
-    update_tags(
-        query_regex=find,
-        query_tag=tag,
-        modify_tag=tag,
-        find_regex=find,
-        replacement=replace,
-        operate_on_albums=operate_on_albums,
-        confirm=False,
-    )
-
-
-strip_bracket_years = (
-    'Removing bracketed years from all "album" tags...',
-    BRACKET_YEAR_REGEX,
-    "",
-    "album",
-    True,
-)
-
-replace_recs_with_recordings = (
-    'Replacing "Rec.s" with "Recordings" in all "album" tags...',
-    r"\bRec\.s\b",
-    "Recordings",
-    "album",
-    True,
-)
-
-strip_solo_instruments = (
-    'Removing "solo" instrument brackets from all "artist" tags...',
-    r"\s\[solo.+\]",
-    "",
-    "artist",
-    False,
-)
+    tags = [
+        (escape(tag), sub(find, replace, tag))
+        for tag in list_items(tag, find, operate_on_albums)
+    ]
+    if tags:
+        for found_value, replacement_value in tags:
+            beet_modify(False, operate_on_albums, tag, found_value, replacement_value)
+        else:
+            echo("No albums to update.")
 
 
 def remove_nonsense_main():
-    for action in [
-        strip_bracket_years,
-        replace_recs_with_recordings,
-        strip_solo_instruments,
-    ]:
+    for action in ACTIONS:
         message, find, replace, tag, operate_on_albums = action
         replace_tags(message, find, replace, tag, operate_on_albums)
