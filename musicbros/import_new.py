d65cc73a7328ea50855643506873aebe9dbce688
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-08-05 22:14:01 -0400

    add ImportError Enum

--- musicbros/import_new.py
+++ musicbros/import_new.py
@@ -4,7 +4,7 @@ from pickle import load
 from re import Match, escape, search, sub
 from typing import Optional
 
-from beets.importer import history_add
+from beets.importer import Enum, history_add
 from rich.prompt import Prompt
 
 from .config import (
@@ -17,6 +17,7 @@ from .library import modify_tracks
 from .regex import BRACKET_DISC_REGEX, BRACKET_SOLO_INSTRUMENT, BRACKET_YEAR_REGEX
 from .style import PrintLevel, print_with_color
 from .tags import (
+    Tracks,
     get_album_title,
     get_album_wide_tag,
     get_albumartist,
@@ -27,28 +28,36 @@ from .tags import (
     get_years,
 )
 
-AUDIO_FILE_TYPES = ("*.mp3", "*.Mp3", "*.m4a", "*.flac", "*.aif*")
-ESCAPE_ERROR = "escape error"
-CONFLICTING_TRACK_TOTALS = "conflicting track totals"
-MISSING_TRACK_TOTAL = "missing track total"
-MISSING_TRACKS = "missing tracks"
-NO_TRACKS = "no tracks"
-WAV_FILES = "wav files"
-SKIP = "skip"
+BeetsQuery = list[str]
+
+
+class ImportError(Enum):
+    ESCAPE_ERROR = "escape error"
+    CONFLICTING_TRACK_TOTALS = "conflicting track totals"
+    MISSING_TRACK_TOTAL = "missing track total"
+    MISSING_TRACKS = "missing tracks"
+    NO_TRACKS = "no tracks"
+    WAV_FILES = "wav files"
+    SKIP = "skip"
+
+
 ERRORS = {
-    ESCAPE_ERROR: "Error parsing path name",
-    CONFLICTING_TRACK_TOTALS: "Album tracks include more than one track total number",
-    MISSING_TRACK_TOTAL: "Album does not contain a track total number",
-    MISSING_TRACKS: "Album is missing tracks",
-    NO_TRACKS: "Folder does not contain supported audio files",
-    WAV_FILES: "Album is in wav format",
+    ImportError.ESCAPE_ERROR: "Error parsing path name",
+    ImportError.CONFLICTING_TRACK_TOTALS: (
+        "Album tracks include more than one track total number"
+    ),
+    ImportError.MISSING_TRACK_TOTAL: "Album does not contain a track total number",
+    ImportError.MISSING_TRACKS: "Album is missing tracks",
+    ImportError.NO_TRACKS: "Folder does not contain supported audio files",
+    ImportError.WAV_FILES: "Album is in wav format",
 }
 IMPORTABLE_ERROR_KEYS = [
-    CONFLICTING_TRACK_TOTALS,
-    MISSING_TRACK_TOTAL,
-    MISSING_TRACKS,
-    WAV_FILES,
+    ImportError.CONFLICTING_TRACK_TOTALS,
+    ImportError.MISSING_TRACK_TOTAL,
+    ImportError.MISSING_TRACKS,
+    ImportError.WAV_FILES,
 ]
+AUDIO_FILE_TYPES = ("*.mp3", "*.Mp3", "*.m4a", "*.flac", "*.aif*")
 
 
 def get_imported_albums() -> set[str]:
@@ -67,8 +76,8 @@ def get_album_directories() -> list[str]:
     return [root for root, dirs, files in walk(shared_directory) if files and not dirs]
 
 
-def get_tracks(album: str) -> list[Path]:
-    audio_files: list[Path] = []
+def get_tracks(album: str) -> Tracks:
+    audio_files: Tracks = []
     for file_type in AUDIO_FILE_TYPES:
         found_files = Path(album).glob(file_type)
         audio_files.extend(found_files)
@@ -80,20 +89,20 @@ def has_wav_tracks(album: str) -> bool:
     return bool(wav_tracks)
 
 
-def get_track_total(tracks: list[Path]) -> tuple[Optional[int], str]:
-    message = ""
+def get_track_total(tracks: Tracks) -> tuple[Optional[int], Optional[ImportError]]:
+    message = None
     track_totals = get_track_totals(tracks)
     track_total: Optional[str] | int = get_album_wide_tag(track_totals)
     if track_total is not None:
         track_total = int(track_total)
     if len(track_totals) > 1:
-        message = CONFLICTING_TRACK_TOTALS
+        message = ImportError.CONFLICTING_TRACK_TOTALS
     elif not track_total:
-        message = MISSING_TRACK_TOTAL
+        message = ImportError.MISSING_TRACK_TOTAL
     return track_total, message
 
 
-def is_ignored_directory(album: str) -> bool:
+def is_in_ignored_directory(album: str) -> bool:
     ignored_directories = get_ignored_directories()
     matching_directories = (
         directory for directory in ignored_directories if directory in album
@@ -135,7 +144,7 @@ def import_wav_files(album: str):
 
 
 def get_artist_and_artist_field_name(
-    tracks: list[Path],
+    tracks: Tracks,
 ) -> tuple[str, str]:
     field = "albumartist"
     artist = ""
@@ -169,7 +178,7 @@ def get_bracket_number(match: Optional[Match[str]]) -> Optional[str]:
     return "".join(numeric_characters)
 
 
-def check_year(tracks: list[Path], album: str, prompt: bool) -> tuple[str, bool]:
+def check_year(tracks: Tracks, album: str, prompt: bool) -> tuple[str, bool]:
     update_year = False
     years = get_years(tracks)
     year = get_album_wide_tag(years)
@@ -191,7 +200,7 @@ def check_year(tracks: list[Path], album: str, prompt: bool) -> tuple[str, bool]
 
 
 def check_disc(
-    tracks: list[Path],
+    tracks: Tracks,
     album: str,
     skip_confirm_disc_overwrite: bool,
     prompt: bool,
@@ -235,17 +244,17 @@ def check_disc(
     return disc, disc_total, update_disc, remove_bracket_disc
 
 
-def has_solo_instrument(artist: str) -> Optional[str]:
+def has_solo_instrument(artist: str) -> bool:
     if not artist:
-        return None
+        return False
     match = search(BRACKET_SOLO_INSTRUMENT, artist)
     if not match:
-        return None
-    return match.group()
+        return False
+    return bool(match.group())
 
 
 def check_artist(
-    tracks: list[Path], skip_confirm_artist_overwrite: bool, prompt: bool
+    tracks: Tracks, skip_confirm_artist_overwrite: bool, prompt: bool
 ) -> tuple[str, bool]:
     update_artist = False
     artists = get_artists(tracks)
@@ -266,26 +275,26 @@ def check_artist(
     return solo_instrument, bool(update_artist)
 
 
-def get_modify_tracks_query(artist: str, field: str, album_title: str) -> list[str]:
+def get_modify_tracks_query(artist: str, field: str, album_title: str) -> BeetsQuery:
     query = [f"album::^{album_title}$"]
     if field and artist:
         query = [f"{field}::^{artist}$"] + query
     return query
 
 
-def get_modify_tracks_modification(field: str, new_value: str) -> list[str]:
+def get_modify_tracks_modification(field: str, new_value: str) -> BeetsQuery:
     return [f"{field}={new_value}"]
 
 
 def import_album(
     album: str,
-    tracks: list[Path],
+    tracks: Tracks,
     import_all: bool,
     as_is: bool,
     skip_confirm_disc_overwrite: bool,
     skip_confirm_artist_overwrite: bool,
     prompt: bool,
-) -> str:
+) -> Optional[ImportError]:
     track_count = len(tracks)
     track_total, track_message = get_track_total(tracks)
     is_complete_album = track_count and track_total and track_count == track_total
@@ -300,8 +309,8 @@ def import_album(
         )
         requires_prompt = not update_year or not update_disc or not update_artist
         if not prompt and requires_prompt:
-            return SKIP
-        error = "" if beet_import(album) else ESCAPE_ERROR
+            return ImportError.SKIP
+        error = None if beet_import(album) else ImportError.ESCAPE_ERROR
         if error or as_is:
             return error
         artist, field = get_artist_and_artist_field_name(tracks)
@@ -334,19 +343,10 @@ def import_album(
     elif track_message:
         error = track_message
     elif isinstance(track_total, int) and track_count > track_total:
-        error = CONFLICTING_TRACK_TOTALS
+        error = ImportError.CONFLICTING_TRACK_TOTALS
     else:
-        error = MISSING_TRACKS
+        error = ImportError.MISSING_TRACKS
     return error
-
-
-def print_errors(errors: dict):
-    for key, error_albums in errors.items():
-        if error_albums:
-            album_string = "Albums" if len(error_albums) > 1 else "Album"
-            print_with_color(f"{album_string} {key}:", style=PrintLevel.INFO)
-            for album in error_albums:
-                print(f"- {album}")
 
 
 def import_albums(
@@ -357,19 +357,17 @@ def import_albums(
     import_all=False,
     prompt=True,
 ):
-    errors: dict[str, list] = {key: [] for key in ERRORS.keys()}
+    errors: dict[ImportError, list] = {key: [] for key in ERRORS.keys()}
     imports = False
     wav_imports = 0
     skipped_count = 0
     prompt_skipped_count = 0
     importable_error_albums = []
+    albums = [album for album in albums if not is_in_ignored_directory(album)]
     for album in albums:
-        if not import_all:
-            if is_ignored_directory(album):
-                continue
-            if is_already_imported(album):
-                skipped_count += 1
-                continue
+        if not import_all and is_already_imported(album):
+            skipped_count += 1
+            continue
         tracks = get_tracks(album)
         wav_tracks = has_wav_tracks(album)
         if tracks:
@@ -397,23 +395,27 @@ def import_albums(
                 history_add([album.encode()])
                 wav_imports += 1
             elif prompt:
-                errors[WAV_FILES].append(album)
+                errors[ImportError.WAV_FILES].append(album)
                 importable_error_albums.append(album)
             else:
                 prompt_skipped_count += 1
         if not tracks and not wav_tracks:
             if prompt:
-                errors[NO_TRACKS].append(album)
+                errors[ImportError.NO_TRACKS].append(album)
             else:
                 prompt_skipped_count += 1
     if wav_imports:
-        print(
-            f"Imported {wav_imports} {'album' if wav_imports == 1 else 'albums'} in WAV"
-            " format."
-        )
+        album_plural = "album" if wav_imports == 1 else "albums"
+        print(f"Imported {wav_imports} {album_plural} in WAV format.")
     if not import_all:
         print(f"Skipped {skipped_count} previously imported albums.")
     if not prompt:
         print(f"Skipped {prompt_skipped_count} albums requiring prompt.")
-    print_errors(errors)
+    for error_name, error_albums in errors.items():
+        if error_albums:
+            album_plural = "Album" if error_albums == 1 else "Albums"
+            error_message = f"{album_plural} {error_name.value}:"
+            print_with_color(error_message, style=PrintLevel.INFO)
+            for album in error_albums:
+                print(f"- {album}")
     return imports, errors, importable_error_albums
