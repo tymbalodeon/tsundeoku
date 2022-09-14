2eb80729b3906a1e95618b605ea52ae28fffb76d
Ben Rosen <benrosen@upenn.edu>
2022-09-14 13:46:28 -0400

    setup conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,27 @@
-from pytest import fixture
+from collections.abc import Callable
 from pathlib import Path
+
+from pytest import fixture
+
+mock_argv = Callable[[], list[str]]
+
+
+def get_mock_get_argv(command: str, short=False) -> mock_argv:
+    if short:
+        help_option = "-h"
+    else:
+        help_option = "--help"
+
+    def mock_get_argv() -> list[str]:
+        return [command, help_option]
+
+    return mock_get_argv
+
+
+def get_mock_get_argvs(command: str) -> tuple[mock_argv, mock_argv]:
+    mock_get_argv_long = get_mock_get_argv(command)
+    mock_get_argv_short = get_mock_get_argv(command, short=True)
+    return mock_get_argv_long, mock_get_argv_short
 
 
 @fixture(autouse=True)
--- tests/test_config.py
+++ tests/test_config.py
@@ -4,21 +4,17 @@ from typer.testing import CliRunner
 from tsundeoku import main
 from tsundeoku.main import tsundeoku
 
-config_help_text = "Show [default] and set config values."
+from .conftest import get_mock_get_argvs
 
-
-def mock_get_argv_long():
-    return ["config", "--help"]
-
-
-def mock_get_argv_short():
-    return ["config", "-h"]
+config_command = "config"
+mock_get_argv_long, mock_get_argv_short = get_mock_get_argvs(config_command)
 
 
 @mark.parametrize(
     "arg, mock_get_argv", [("--help", mock_get_argv_long), ("-h", mock_get_argv_short)]
 )
 def test_config_help(arg, mock_get_argv, monkeypatch):
+    config_help_text = "Show [default] and set config values."
     monkeypatch.setattr(main, "get_argv", mock_get_argv)
-    result = CliRunner().invoke(tsundeoku, ["config", arg])
-    assert config_help_text in result.output
+    output = CliRunner().invoke(tsundeoku, [config_command, arg]).output
+    assert config_help_text in output
