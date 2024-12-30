786acd8a1606d68c4323867c1eb333b4b099bb4f
Ben Rosen <benjamin.j.rosen@gmail.com>
2024-12-29 20:39:47 -0500

    feat: improve rust

--- src/main.rs
+++ src/main.rs
@@ -2,86 +2,114 @@ use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 
-/// Import audio files from a shared folder to a local library
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
+#[derive(Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+enum Config {
+    /// Open config file in $EDITOR
+    Edit,
 
-    #[command(subcommand)]
-    command: Option<Commands>,
+    /// Show config file path
+    Path,
+
+    /// Set config values
+    Set,
+
+    /// Show config values
+    Show,
+}
+
+#[derive(Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+enum Schedule {
+    /// Enabled scheduled imports
+    Enable {
+        #[arg(long, value_name = "TIME")]
+        time: Option<String>,
+    },
+
+    /// Disable scheduled imports
+    Disable,
+
+    /// Show schedule status
+    Status,
 }
 
 #[derive(Subcommand)]
 enum Commands {
     /// Show and set config values
     Config {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
+        #[command(subcommand)]
+        command: Option<Config>,
     },
 
     /// Import newly added audio files from shared folders to a local folder
     Import {
+        #[arg(long, value_name = "FILE")]
+        shared_dirs: Option<PathBuf>,
+
+        #[arg(long, value_name = "FILE")]
+        ignored_paths: Option<PathBuf>,
+
+        #[arg(long, value_name = "FILE")]
+        local_dir: Option<PathBuf>,
+
         #[arg(short, long)]
-        list: bool,
+        force: bool,
     },
 
     /// Show import logs
-    Logs {
-        #[arg(short, long)]
-        list: bool,
-    },
+    Logs,
 
     /// Enable and disable scheduled imports
     Schedule {
-        #[arg(short, long)]
-        list: bool,
+        #[command(subcommand)]
+        command: Option<Schedule>,
     },
+}
+
+/// Import audio files from a shared folder to a local folder
+#[derive(Parser)]
+#[command(arg_required_else_help = true)]
+#[command(version)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    #[arg(long, value_name = "FILE")]
+    config_file: Option<PathBuf>,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    if let Some(config_path) = cli.config.as_deref() {
+    if let Some(config_path) = cli.config_file.as_deref() {
         println!("Value for config: {}", config_path.display());
     }
 
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
     match &cli.command {
-        Some(Commands::Config { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
+        Some(Commands::Config { command }) => if let Some(command) = command {
+            println!("{command:?} is not yet implemented.");
+        },
+
+        Some(Commands::Import {
+            shared_dirs,
+            ignored_paths,
+            local_dir,
+            force,
+        }) => {
+            println!(
+                "{shared_dirs:?}, {ignored_paths:?}, {local_dir:?}, {force:?}"
+            );
         }
-        Some(Commands::Import { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
+
+        Some(Commands::Logs) => {
+            println!("Logs is not yet implemented.");
         }
-        Some(Commands::Logs { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        Some(Commands::Schedule { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
+
+        Some(Commands::Schedule { command }) => if let Some(command) = command {
+            println!("{command:?} is not yet implemented.");
+        },
+
         None => {}
     }
-
-    // Continued program logic goes here...
 }
